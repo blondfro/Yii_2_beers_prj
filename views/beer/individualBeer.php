801a8e9d1bce0d00db40d688ba31ca6df46a1499
James Morris <blondfro@gmail.com>
2017-04-26 10:48:23 -0500

    Updating views for _forms and individualBeer.  Also adjusted the css file to adjust the panel header text size.

--- views/beer/individualBeer.php
+++ views/beer/individualBeer.php
@@ -25,28 +25,209 @@ $this->params['breadcrumbs'][] = $this->title;
         ]) ?>
     </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            //'id',
-            'beer_name',
-            'brewery_name',
-            'beer_type',
-            'beer_abv',
-            'beer_ibu',
-            'comment',
-            'venue_name',
-            'venue_city',
-            'venue_state',
-            'venue_lat',
-            'venue_lng',
-            'rating_score',
-            'created_at',
-            'checkin_url:url',
-            'beer_url:url',
-            'brewery_url:url',
-            'brewery_country',
-        ],
-    ]) ?>
+    <div class="panel panel-default">
+        <div class="panel-heading">Beer Details</div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'beer_name'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'beer_type'
+                                                    ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'beer_abv'
+                        ],
+                    ]) ?>
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'beer_ibu'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'beer_url'
+                        ],
+                    ]) ?>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'brewery_name'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'brewery_country'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'brewery_url'
+                        ],
+                    ]) ?>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'comment'
+                        ],
+                    ]) ?>
+                </div>
+
+            </div>
+
+
+        </div>
+    </div>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Venue Details</h3>
+        </div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'venue_name'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'venue_city'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'venue_state'
+                        ],
+                    ]) ?>
+                </div>
+
+
+            </div>
+
+            <div class="row">
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'venue_lat'
+                        ],
+                    ]) ?>
+                </div>
+
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'venue_lng'
+                        ],
+                    ]) ?>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Miscellaneous Details</h3>
+        </div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'rating_score'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'created_at'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            //'id',
+                            'checkin_url:url'
+                        ],
+                    ]) ?>
+                </div>
+
+
+            </div>
+
+        </div>
+    </div>
 
 </div>
