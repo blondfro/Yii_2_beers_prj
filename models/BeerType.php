1fc0b68bc920d3ef5a2fd2119542a120f26ce5d6
James Morris <blondfro@gmail.com>
2017-04-28 12:13:23 -0500

    Added model for the beer_type table

---
+++ models/BeerType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "beer_type".
+ *
+ * @property integer $id
+ * @property string $name
+ */
+class BeerType extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'beer_type';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+}
