5cad5b185115b7c6049c8378ccf0bc4f1134169c
James Morris <blondfro@gmail.com>
2017-05-10 11:58:09 -0500

    Formatting Beer View with panels for easier layout.

--- views/beer/view.php
+++ views/beer/view.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 
+
 /* @var $this yii\web\View */
 /* @var $model app\models\Beer */
 
@@ -25,26 +26,140 @@ $this->params['breadcrumbs'][] = $this->title;
         ]) ?>
     </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            // 'id',
-            'beer_name',
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">Beer Details</h3>
+        </div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            // 'id',
+                            'beer_name'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            // 'beer_type',
+                            // 'beer_type_id',
+                            'beerType.name'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'beer_abv'
+                        ],
+                    ]) ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'beer_ibu',
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'rating_score'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'brewery_id'
+                        ],
+                    ]) ?>
+                </div>
+            </div>
 
-            // 'beer_type',
-            // 'beer_type_id',
-            'beerType.name',
+            <div class="row">
+                <div class="col-md-8">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'comment'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'rating_score',
+                        ],
+                    ]) ?>
+                </div>
+            </div>
 
-            'beer_abv',
-            'beer_ibu',
-            'comment',
-            'rating_score',
-            'created_at:datetime',
-            'checkin_url:url',
-            'beer_url:url',
-            'brewery_id',
-            'venue_id',
-        ],
-    ]) ?>
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Additional Beer Details</div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'brewery_id'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'venue_id',
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'beer_url:url'
+                        ],
+                    ]) ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'created_at:datetime'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                           'checkin_url:url'
+                        ],
+                    ]) ?>
+                </div>
+                <div class="col-md-4">
+
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div>
