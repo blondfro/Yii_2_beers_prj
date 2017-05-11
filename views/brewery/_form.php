74c55edd2b04080ed0552c2478107b5b4de69582
James Morris <blondfro@gmail.com>
2017-05-11 11:39:04 -0500

    Changed _forms for brewery to display the country name instead of it's id.

--- views/brewery/_form.php
+++ views/brewery/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\models\Country;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Brewery */
@@ -11,12 +13,27 @@ use yii\widgets\ActiveForm;
 <div class="brewery-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
+<!--
     <?= $form->field($model, 'countryId')->textInput() ?>
-
+-->
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
+    <?= $form->field($model, 'countryId')
+        ->dropDownList(
+            ArrayHelper::map(
+                Country::find()
+                    ->asArray()
+                    ->orderBy('name')
+                    ->all(),
+                'id', 'name'),
+            [
+                'prompt' => 'What country is this brewery in',
+            ])
+        ->label('Venue');
+    ?>
+
     <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
+
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
