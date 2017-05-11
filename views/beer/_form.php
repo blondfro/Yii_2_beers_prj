0d84d27535476a1d23716bcfd76cc90e83046030
James Morris <blondfro@gmail.com>
2017-05-11 11:57:09 -0500

    updated beer _form to have panel layout

--- views/beer/_form.php
+++ views/beer/_form.php
@@ -13,105 +13,164 @@ use yii\helpers\ArrayHelper;
 ?>
 
 <div class="beer-form">
-
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'beer_name')
-        ->textInput(['maxlength' => true])
-    ?>
+    <div class="panel panel-primary">
+        <div class="panel-heading">Beer Details</div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-4">
+                    <?= $form->field($model, 'beer_name')
+                        ->textInput(['maxlength' => true])
+                    ?>
+                </div>
+                <div class="col-md-4">
+                    <?= $form->field($model, 'beer_type_id')
+                        ->dropDownList(
+                            ArrayHelper::map(
+                                BeerType::find()
+                                    ->asArray()
+                                    ->orderBy('name')
+                                    ->all(),
+                                'id', 'name'),
+                            [
+                                'prompt' => 'What type of beer is this?',
+                            ])
+                        ->label('Beer type');
+                    ?>
+                </div>
+                <div class="col-md-4">
+                    <?= $form->field($model, 'beer_abv')
+                        ->textInput([
+                            'maxlength' => true,
+                        ])
+                    ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <?= $form->field($model, 'beer_ibu')
+                        ->textInput()
+                    ?>
+                </div>
+                <div class="col-md-4">
+                    <?= $form->field($model, 'rating_score')
+                        ->textInput([
+                            'maxlength' => true,
+                        ])
+                    ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <?= $form->field($model, 'comment')
+                        ->textInput([
+                            'maxlength' => true,
+                        ])
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Additional Details</div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-4">
+                    <?= $form->field($model, 'brewery_id')
+                        ->dropDownList(
+                            ArrayHelper::map(
+                                Brewery::find()
+                                    ->asArray()
+                                    ->orderBy('name')
+                                    ->all(),
+                                'id', 'name'),
+                            [
+                                'prompt' => 'What brewery does this come from',
+                            ])
+                        ->label('Brewery');
+                    ?>
+                </div>
+                <div class="col-md-4">
+                    <?= $form->field($model, 'venue_id')
+                        ->dropDownList(
+                            ArrayHelper::map(
+                                Venue::find()
+                                    ->asArray()
+                                    ->orderBy('name')
+                                    ->all(),
+                                'id', 'name'),
+                            [
+                                'prompt' => 'What Venue will thies be at?',
+                            ])
+                        ->label('Venue');
+                    ?>
+                </div>
+                <div class="col-md-4">
+                    <?= $form->field($model, 'beer_url')
+                        ->textInput([
+                            'maxlength' => true,
+                        ])
+                    ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <?= $form->field($model, 'created_at')
+                        ->textInput()
+                    ?>
+                </div>
+                <div class="col-md-4">
+                    <?= $form->field($model, 'checkin_url')
+                        ->textInput([
+                            'maxlength' => true,
+                        ])
+                    ?>
+                </div>
+                <div class="col-md-4">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
 <!--
     <? $form->field($model, 'beer_type')->
     textInput(['maxlength' => true]) ?>
     <? $form->field($model, 'beer_type_id')->textInput() ?>
     -->
-    <?= $form->field($model, 'beer_type_id')
-        ->dropDownList(
-            ArrayHelper::map(
-                BeerType::find()
-                    ->asArray()
-                    ->orderBy('name')
-                    ->all(),
-                'id', 'name'),
-            [
-                'prompt' => 'What type of beer is this?',
-            ])
-        ->label('Beer type');
-    ?>
 
-    <?= $form->field($model, 'beer_abv')
-        ->textInput([
-            'maxlength' => true,
-        ])
-    ?>
 
-    <?= $form->field($model, 'beer_ibu')
-        ->textInput()
-    ?>
 
-    <?= $form->field($model, 'comment')
-        ->textInput([
-            'maxlength' => true,
-        ])
-    ?>
 
-    <?= $form->field($model, 'rating_score')
-        ->textInput([
-            'maxlength' => true,
-        ])
-    ?>
 
-    <?= $form->field($model, 'created_at')
-        ->textInput()
-    ?>
 
-    <?= $form->field($model, 'checkin_url')
-        ->textInput([
-            'maxlength' => true,
-        ])
-    ?>
 
-    <?= $form->field($model, 'beer_url')
-        ->textInput([
-            'maxlength' => true,
-        ])
-    ?>
+
+
+
+
+
+
+
+
 <!--
     <?= $form->field($model, 'brewery_id')
         ->textInput()
     ?>
 -->
-    <?= $form->field($model, 'brewery_id')
-        ->dropDownList(
-            ArrayHelper::map(
-                Brewery::find()
-                    ->asArray()
-                    ->orderBy('name')
-                    ->all(),
-                'id', 'name'),
-            [
-                'prompt' => 'What brewery does this come from',
-            ])
-        ->label('Brewery');
-    ?>
+
 
     <!--
     <?= $form->field($model, 'venue_id')
         ->textInput()
     ?>
     -->
-    <?= $form->field($model, 'venue_id')
-        ->dropDownList(
-            ArrayHelper::map(
-                Venue::find()
-                    ->asArray()
-                    ->orderBy('name')
-                    ->all(),
-                'id', 'name'),
-            [
-                'prompt' => 'What Venue will thies be at?',
-            ])
-        ->label('Venue');
-    ?>
+
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
