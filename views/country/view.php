562c4093a0cdcaad96149bea042896b72da91478
James Morris <blondfro@gmail.com>
2017-06-28 10:29:27 -0500

    hid the id column on the list of breweries in a country.

--- views/country/view.php
+++ views/country/view.php
@@ -41,10 +41,7 @@ $this->params['breadcrumbs'][] = $this->title;
         // 'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
             'name',
-
             ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
